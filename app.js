af0f00174191f26fd0528c53dd36156fcd78ccfa
1230439 <arnau.torrents@est.fib.upc.edu>
2021-01-04 11:37:06 +0100

    REST initialized

--- app.js
+++ app.js
@@ -5,7 +5,23 @@ const port = 3000;
 app.get('/', (req,res) => {
     // req es la request, la peticion
     // res  es la respuesta
-    res.send('Hello World!');
+    res.status(200).send('Hello World!');
+});
+
+app.post('/team/pokemons', () => {
+    res.status(200).send('Hello World!');
+});
+
+app.get('/team', () => {
+    res.status(200).send('Hello World!');
+});
+
+app.delete('/team/pokemons/:pokeid', () => {
+    res.status(200).send('Hello World!');
+});
+
+app.put('/team', () => {
+    res.status(200).send('Hello World!');
 });
 
 app.listen(port, () => {
