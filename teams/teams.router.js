17d994e6bf306acd0bd40f05ffa6ee6f1bc0ec0d
arnautfdez <arnau.torrents.fernandez@gmail.com>
2021-01-25 16:35:10 +0100

    refactor: middlewares

--- teams/teams.router.js
+++ teams/teams.router.js
@@ -1,30 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-require('../tools/auth')(passport);
 const axios = require('axios').default;
 
 const teamsController = require('./teams.controller');
 const { getUser } = require('../auth/users.controller');
 
 router.route('/')
-    .get(passport.authenticate('jwt', {session: false}),
-        (req, res, next) => {
+    .get((req, res) => {
             let user = getUser(req.user.userId);
             res.status(200).json({
                 trainer: user.userName,
                 team: teamsController.getTeamOfUser(req.user.userId)
             })
         })
-    .put(passport.authenticate('jwt', {session: false}),
-        (req, res) => {
+    .put((req, res) => {
         teamsController.setTeam(req.user.userId, req.body.team);
         res.status(200).send();
     })
 
 router.route('/pokemons')
-    .post(passport.authenticate('jwt', {session: false}),
-        (req, res) => {
+    .post((req, res) => {
         let pokemonName = req.body.name;
         axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
             .then(function (response) {
@@ -47,8 +42,7 @@ router.route('/pokemons')
     })
 
 router.route('/pokemons/:pokeid')
-    .delete(passport.authenticate('jwt', {session: false}),
-        (req, res) => {
+    .delete((req, res) => {
         teamsController.deletePokemonAt(req.user.userId, req.params.pokeid);
         res.status(200).send();
     })
